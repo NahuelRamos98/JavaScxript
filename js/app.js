1e799fe159ccf556b3b65cd1b067a6daccdd8e48
Nahuel Ramos <99888741+NahuelRamos98@users.noreply.github.com>
2022-09-22 21:28:49 -0300

    agregamos librerias

--- js/app.js
+++ js/app.js
@@ -1,4 +1,41 @@
+/* Lista De Usuarios */
+
+let listaDeUsuarios = [{
+    nombre: "Nahuel",
+    edad: 24,
+    horasDeEstudio: 4,
+    diasDeEstudio: ["Lunes, Miercoles, Viernes"]
+},{
+    nombre: "Jorge",
+    edad: 20,
+    horasDeEstudio: 2,
+    diasDeEstudio: ["Lunes, Viernes"]
+},{
+    nombre: "Miguel",
+    edad: 31,
+    horasDeEstudio: 5,
+    diasDeEstudio: ["Lunes, Miercoles, Viernes"]
+},{
+    nombre: "Nicolas",
+    edad: 26,
+    horasDeEstudio: 1,
+    diasDeEstudio: ["Lunes, Miercoles, Viernes, Sabado"]
+}]
+
+localStorage.setItem("Usuarios",JSON.stringify(listaDeUsuarios))
+
+listaDeUsuarios = JSON.parse(localStorage.getItem("Usuarios"))
+
+listaDeUsuarios.forEach(element =>{
+    let elements = document.getElementById("usuRecientes");
+    let div = document.createElement("div");
+    div.classList.add("listadiv")
+    div.innerHTML ="Nombre: "+ element.nombre + "<br>Edad: "+ element.edad+ "<br>Horas de estudio: "+ element.horasDeEstudio + "<br>Dias de estudio: "+ element.diasDeEstudio;
+    elements.appendChild(div);
+});
+
 //POMO//
+
 function conjuntoDeVariables() {
      workTime = workTimerInput.value; 
      breakTime = breakTimerInput.value;
@@ -34,12 +71,33 @@ function startTime() {
 startButton.onclick = () => {
     conjuntoDeVariables();
     startTime();
+    toastr["info"]("Si no vas hasta el final ¿por qué empezar?", "Recuerda...")
 };
 
+
+
 // TIMER //
+
 let currentTime = 1;
 let seconds = 0;
+let = clockMinutes;
+let = clockSeconds;
 
+function updateClock() {
+    clockMinutes = formatNumbers(currentTime);
+    clockSeconds = formatNumbers(seconds);
+    clock.innerHTML = clockMinutes + ":" + clockSeconds;
+}
+
+function formatNumbers(time) {
+    let formattedDigits;
+    if (time < 10) {
+        formattedDigits = "0" + time;
+    } else {
+        formattedDigits = time;
+    }
+    return formattedDigits;
+}
 function timer() {
     if(currentTime > 0 || seconds > 0){
     if(seconds == 0){
@@ -48,6 +106,7 @@ function timer() {
     } else{
         seconds--;
     }
+    updateClock();
     console.log(currentTime, seconds);
     interval = setTimeout(timer, 1000);
 } else {
@@ -73,43 +132,10 @@ function pomodoroController() {
         timer();
         console.log("Tiempo de trabajar TC : " + timesCompleted + " Ciclos: " + cyclesCompleted);
     } else {
+        toastr["warning"]("Tiempo de descansar", "Uff")
         currentTime = breakTime;
         timesCompleted++;
         timer();
         console.log("Tiempo de descanso TC : " + timesCompleted + " Ciclos: " + cyclesCompleted);
     }
 }
-
-let listaDeUsuarios = [{
-    nombre: "Nahuel",
-    edad: 24,
-    horasDeEstudio: 4,
-    diasDeEstudio: ["Lunes, Miercoles, Viernes"]
-},{
-    nombre: "Jorge",
-    edad: 20,
-    horasDeEstudio: 2,
-    diasDeEstudio: ["Lunes, Viernes"]
-},{
-    nombre: "Miguel",
-    edad: 31,
-    horasDeEstudio: 5,
-    diasDeEstudio: ["Lunes, Miercoles, Viernes"]
-},{
-    nombre: "Nicolas",
-    edad: 26,
-    horasDeEstudio: 1,
-    diasDeEstudio: ["Lunes, Miercoles, Viernes, Sabado"]
-}]
-
-localStorage.setItem("Usuarios",JSON.stringify(listaDeUsuarios))
-
-listaDeUsuarios = JSON.parse(localStorage.getItem("Usuarios"))
-
-listaDeUsuarios.forEach(element =>{
-    let elements = document.getElementById("usuRecientes");
-    let div = document.createElement("div");
-    div.classList.add("listadiv")
-    div.innerHTML ="Nombre: "+ element.nombre + "<br>Edad: "+ element.edad+ "<br>Horas de estudio: "+ element.horasDeEstudio + "<br>Dias de estudio: "+ element.diasDeEstudio;
-    elements.appendChild(div);
-})
--- js/form.js
+++ js/form.js
@@ -1,14 +1,23 @@
 console.log("USUARIOS: NAHUEL  CONTRASEÑA: 1234567")
 console.log("OTRO USUARIO : Agustin  Contraseña:123789")
-
+ 
 function entrar() {
      usu = document.getElementById("usuarioPrin").value;
      pass = document.getElementById("passPrin").value;
 
+    
+
     if (usu == 'Nahuel' && pass == '1234567' || usu == 'Agustin' && pass == '123789') {
-        window.location.href = "views/index.html"
-        localStorage.setItem("Usuario", usu);
-    }else{
-        alert("Nombre de usuario y contraseña equivocado")
-    }
+        setTimeout(function(){
+            window.location.href = "views/index.html"
+        }, 5000);
+            
+            toastr["success"]("Espere 5 segundos y será redireccionado", "Acceso concedido");
+
+            localStorage.setItem("Usuario", usu);
+
+        }else{
+                toastr["error"]("Nombre de usuario y/o contraseña incorrecto/a.", "Error")
+    };
 }
+
